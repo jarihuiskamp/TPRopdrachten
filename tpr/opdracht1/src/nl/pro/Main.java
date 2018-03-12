3658d06bf3021b81a4f447a27826df1eff993121
jarihuiskamp <30985275+jarihuiskamp@users.noreply.github.com>
2018-03-12 13:26:50 +0100

    Add files via upload

---
+++ tpr/opdracht1/src/nl/pro/Main.java
@@ -0,0 +1,44 @@
+package nl.pro;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Opdracht 1.1:");
+        System.out.println("Java is Object Georiënteerd");
+        System.out.println("Java is strongly typed");
+        System.out.println("Java is geen JavaScript");
+        System.out.println("");
+        System.out.println("Opdracht 1.2:");
+        System.out.println("Java is geweldig!");
+        System.out.println("Java is geweldig!");
+        System.out.println("Java is geweldig!");
+        System.out.println("Java is geweldig!");
+        System.out.println("Java is geweldig!");
+        System.out.println("");
+        System.out.println("Opdracht 1.3:");
+        System.out.println("a    a^2    a^3    a^4");
+        System.out.println("1    1      1      1");
+        System.out.println("2    4      8      16");
+        System.out.println("3    9      27     81");
+        System.out.println("4    16     64     256");
+        System.out.println("");
+        System.out.println("Opdracht 1.4:");
+        System.out.println((7.5 * 6.5 - 4.5 * 3) / (47.5 * 5.5));
+        System.out.println("");
+        System.out.println("Opdracht 1.5:");
+        System.out.println(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10);
+        System.out.println("");
+        System.out.println("Opdracht 1.6:");
+        System.out.println(4 * (1.0 - 1.0/3 + 1.0/5 - 1.0/7 + 1.0/9 - 1.0/11 + 1.0/13));
+        System.out.println("");
+        System.out.println("Opdracht 1.7:");
+        System.out.println("oppervlakte = " + 6.5 * 6.5 * 3.14159);
+        System.out.println("omtrek = " + 2 * 6.5 * 3.14159);
+        System.out.println("");
+        System.out.println("Opdracht 1.8:");
+        System.out.println("oppervlakte = " + 5.3 * 8.6);
+        System.out.println("omtrek = " + 2 * (5.3 + 8.6));
+
+
+}
+}
